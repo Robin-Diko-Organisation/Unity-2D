82e1cd2fc5b04525c1cc20feaa8122e8091d86d4
Robin Mertens <robin.mertens@udo.edu>
2020-03-01 17:10:02 +0100

    MainMenu Animations and Translations fixed

---
+++ RPG-2D/Assets/Scripts/Buttons/Btn_QuitGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Animations;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Btn_QuitGame : MonoBehaviour, IPointerClickHandler
+{
+    public RuntimeAnimatorController AnimatorController;
+    public string AnimationName;
+    private Animator Animator;
+
+    void Start()
+    {
+        Animator = gameObject.AddComponent(typeof(Animator)) as Animator;
+        Animator.runtimeAnimatorController = AnimatorController;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Animator.Play(AnimationName);
+        float animtime = Animator.GetCurrentAnimatorStateInfo(0).length;
+        Invoke("QuitGame", animtime + 0.5f);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ RPG-2D/Assets/Scripts/Buttons/Btn_Sounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Btn_Sounds : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+{
+    [SerializeField] public AudioSource ButtonClickSound;
+    [SerializeField] public AudioSource ButtonHoverSound;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ButtonClickSound.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ButtonHoverSound.Play();
+    }
+}
