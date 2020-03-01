f740d3d952a7578780f7b53a7d88ac1ca7665dcf
Robin Mertens <robin.mertens@udo.edu>
2020-03-01 03:32:59 +0100

    Button Animation StartMenu

---
+++ RPG-2D/Assets/Scripts/ImageAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageAnimator : MonoBehaviour
+{
+    [SerializeField] private bool isImageNotSprite = true;
+    [SerializeField] private Sprite[] frameArray = null;
+    [SerializeField] private float framerate = 0.1f;
+    private int currentFrame = 0;
+    private float timer = 0;
+    Image image;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        if (isImageNotSprite)
+        {
+            image = gameObject.GetComponent<Image>();
+        }
+        else
+        {
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= framerate)
+        {
+            timer -= framerate;
+            currentFrame = (currentFrame +1) % frameArray.Length;
+            image.sprite = frameArray[currentFrame];
+        }
+    }
+}
