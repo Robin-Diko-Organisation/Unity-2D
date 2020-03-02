0635520baf9a8376194036792ac9c04935eb9845
Robin Mertens <robin.mertens@udo.edu>
2020-03-02 19:44:21 +0100

    Startmenu resized buttons (1/4 of fullHD) added + transitions fixed

---
+++ RPG-2D/Assets/Scripts/Buttons/Btn_Clicked.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Btn_Clicked : MonoBehaviour, IPointerClickHandler
+{
+    public string TriggerName;
+    private Animator Animator;
+
+    void Start()
+    {
+        Animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Animator.SetTrigger(TriggerName);
+    }
+}
